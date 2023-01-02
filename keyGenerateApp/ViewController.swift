8c4840bfd162f7eeb95543bc3f2f1be89d214cf0
Oguzhan <ogigogi56@gmail.com>
2023-01-02 15:37:38 +0300

    Initial Commit

---
+++ keyGenerateApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  keyGenerateApp
+//
+//  Created by oğuzhan salkım on 2.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
